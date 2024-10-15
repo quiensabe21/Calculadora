dd75952e6dd903b8543cd023bb77c94a06fee7fc
Santispinil12 <santiagocortespi@hotmail.com>
2024-10-14 19:47:46 -0500

    creacion de los botones +,-,/ y arreglos

--- calculadora.py
+++ calculadora.py
@@ -31,46 +31,84 @@ Label_entrada1.grid(column =0, row=0, columnspan= 4, sticky=(W, E))
 
 entrada2 = StringVar()
 label_entrada2 = ttk.Label(mainframe,textvariable=entrada2, style="Label2.TLabel")
-label_entrada2.grid(column =0, row=5, columnspan= 4, sticky=(W, E))
+label_entrada2.grid(column =0, row=1, columnspan= 4, sticky=(W, E))
 
 estilos_botones_numeros = ttk.Style()
 estilos_botones_numeros.configure('Botones_numeros.TButton')
-estilos_botones_numeros.map('Botones_numeros. TButton',foreground =[{'active','red'}], backgroung =[{'active', 'black'}])
+# estilos_botones_numeros.map('Botones_numeros. TButton',foreground =[{'active','red'}], backgroung =[{'active', 'black'}])
+estilos_botones_numeros.map(
+    'Botones_numeros.TButton',
+    foreground=[('active', 'red')],
+    background=[('active', 'black')]
+)
 
 esilos_botones_borrar =ttk.Style()
 
 
 
-button0 = ttk.Button(mainframe, text = "0", style="Botones_numeros.TButton")
-button1 = ttk.Button(mainframe, text = "1", style="Botones_numeros.TButton")
-button2 = ttk.Button(mainframe, text = "2", style="Botones_numeros.TButton")
-button3 = ttk.Button(mainframe, text = "3", style="Botones_numeros.TButton")
-button4 = ttk.Button(mainframe, text = "4", style="Botones_numeros.TButton")
-button5 = ttk.Button(mainframe, text = "5", style="Botones_numeros.TButton")
-button6 = ttk.Button(mainframe, text = "6", style="Botones_numeros.TButton")
-button7 = ttk.Button(mainframe, text = "7", style="Botones_numeros.TButton")
-button8 = ttk.Button(mainframe, text = "8", style="Botones_numeros.TButton")
-button9 = ttk.Button(mainframe, text = "9", style="Botones_numeros.TButton")
+button0 = ttk.Button(mainframe, text = "0", style="Botones_numeros.TButton",command=lambda:ingresarvalor('0'))
+button1 = ttk.Button(mainframe, text = "1", style="Botones_numeros.TButton",command=lambda:ingresarvalor('1'))
+button2 = ttk.Button(mainframe, text = "2", style="Botones_numeros.TButton",command=lambda:ingresarvalor('2'))
+button3 = ttk.Button(mainframe, text = "3", style="Botones_numeros.TButton",command=lambda:ingresarvalor('3'))
+button4 = ttk.Button(mainframe, text = "4", style="Botones_numeros.TButton",command=lambda:ingresarvalor('4'))
+button5 = ttk.Button(mainframe, text = "5", style="Botones_numeros.TButton",command=lambda:ingresarvalor('5'))
+button6 = ttk.Button(mainframe, text = "6", style="Botones_numeros.TButton",command=lambda:ingresarvalor('6'))
+button7 = ttk.Button(mainframe, text = "7", style="Botones_numeros.TButton",command=lambda:ingresarvalor('7'))
+button8 = ttk.Button(mainframe, text = "8", style="Botones_numeros.TButton",command=lambda:ingresarvalor('8'))
+button9 = ttk.Button(mainframe, text = "9", style="Botones_numeros.TButton",command=lambda:ingresarvalor('9'))
 
-button_borrar = ttk.Button(mainframe, text = chr(9003))
-button_borrar_todo = ttk.Button(mainframe, text = "C")
+button_borrar = ttk.Button(mainframe, text = chr(9003),command=lambda:borrar())
+button_borrar_todo = ttk.Button(mainframe, text = "C",command=lambda:borrartodo())
 button_parentesis1 = ttk.Button(mainframe, text = "(")
 button_parentesis2 = ttk.Button(mainframe, text = ")")
 button_punto = ttk.Button(mainframe, text = ".")
 
 
-button_division = ttk.Button (mainframe, text=chr(247)) 
-button_multiplicacion= ttk.Button(mainframe, text="x")
-button_resta =ttk. Button (mainframe, text="-")
-button_suma = ttk.Button (mainframe, text="+")
+button_division = ttk.Button (mainframe, text=chr(247),command=lambda: ingresarvalor('/')) 
+button_multiplicacion= ttk.Button(mainframe, text="x",command=lambda:ingresarvalor('*'))
+button_resta =ttk. Button (mainframe, text="-",command=lambda:ingresarvalor('-'))
+button_suma = ttk.Button (mainframe, text="+",command=lambda:ingresarvalor('+'))
 
 
-button_igual = ttk. Button (mainframe, text="=") 
-button_raiz_cuadrada = ttk. Button (mainframe, text="√")
+button_igual = ttk. Button (mainframe, text="=",command=lambda:ingresarvalor('=')) 
+button_raiz_cuadrada = ttk. Button (mainframe, text="√",command=lambda:raizcuadrada())
 
+def ingresarvalor(tecla):
+    if tecla >= '0' and tecla <= '9' or tecla =='(' or tecla ==')' or tecla =='.':
+        entrada2.set(entrada2.get()+tecla)
+
+    if tecla == '*' or tecla =='/' or tecla == '+'or tecla == '-':
+        if tecla=='*':
+            entrada1.set(entrada2.get()+'*')
+        elif tecla == '/':
+            entrada1.set(entrada2.get()+'/')
+        elif tecla =='+':
+             entrada1.set(entrada2.get()+'+')
+        elif tecla =='-':
+            entrada1.set(entrada2.get()+'-')
+            
+        entrada2.set('')
+
+    if tecla=='=':
+        entrada1.set(entrada1.get()+ entrada2.get())  
+        resultado = eval(entrada1.get()) 
+        entrada2.set(resultado)
+
+def raizcuadrada():
+    entrada1.set('')
+    resultado =math.sqrt(float(entrada2.get()))
+    entrada2.set(resultado)
+    
+def borrar():
+    inicio = 0
+    final = len(entrada2.get()) #150
+    
+    entrada2.set(entrada2.get()[inicio:final-1])
+    
+def borrartodo():
+    entrada1.set('')
+    entrada2.set('')    
 #Botones en pantalla
-
-
 
 button_parentesis1.grid(column=0, row=2)
 button_parentesis2.grid(column=1, row=2)
@@ -102,7 +140,6 @@ button_raiz_cuadrada.grid(column=3, row=7)
 
 
 root, mainloop()
-
 
 
 
